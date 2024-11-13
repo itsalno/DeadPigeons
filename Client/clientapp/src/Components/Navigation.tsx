d7a0a4dd0dc95dd46f613ec9057e4be179a30fac
alekss <anoviconoks@gmail.com>
2024-11-13 14:02:14 +0100

    Added starting components for the app and changed Navigation.tsx

--- Client/clientapp/src/Components/Navigation.tsx
+++ Client/clientapp/src/Components/Navigation.tsx
@@ -5,11 +5,18 @@ export default function Navigation() {
     return (
         <div className="navbar bg-base-100 h-16 min-h-[4rem]">
             <div className="flex-1">
-                <Link to="/" className="btn btn-ghost small-case text-xl m-2">Home</Link>
+                <Link to="/" className="btn btn-ghost small-case text-l m-1">Home</Link>
+                <Link to ="/Users" className="btn btn-ghost small-case text-l m-1">Users</Link>
+                <Link to ="/Games" className="btn btn-ghost small-case text-l m-1">Play</Link>
+                <Link to ="/History" className="btn btn-ghost small-case text-l m-1">History of games</Link>
+                <Link to ="/Balance" className="btn btn-ghost small-case text-l m-1">Balance</Link>
 
             </div>
             <div className="flex-none">
                 <ThemeSwitcher/>
+            </div>
+            <div className="flex-none">
+                <Link to="/LogIn" className="btn btn-ghost small-case text-l m-1">Log In</Link>
             </div>
         </div>
     );
